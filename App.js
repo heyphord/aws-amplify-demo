ec4bb50c6c9a71f6568351761b0afe04f93fd7da
heyphord <apps.heyphord@gmail.com>
2023-08-13 15:36:17 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,88 @@
+import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native';
+//aws amplify imports
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { createBlog } from "./src/graphql/mutations";
+import { listBlogs } from "./src/graphql/queries";
+
+import awsconfig from './aws-exports';
+import React from 'react';
+Amplify.configure(awsconfig);
+
+
+
+export default function App() {
+
+  const [isLoading , setLoading] = React.useState(false);
+  const [blogs , setBlogs] = React.useState([1,2,3]);
+  const [error , setError] = React.useState({});
+
+  const addBlogFxn = async()=>{
+    try {
+      
+      setLoading(true);
+      const result = await API.graphql(graphqlOperation(createBlog, {
+        input: {
+          name: 'My first blog!'
+        }
+      }))
+  
+      console.log(result);
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      setError(error)
+      console.log(error);
+      
+    }
+  }
+  
+  const listBlogsFxn = async()=>{
+    
+    try {
+      setLoading(true)
+      const result = await API.graphql(graphqlOperation(listBlogs))
+      console.log(result)
+      setLoading(false);
+      
+    } catch (error) {
+      setLoading(false);
+      setError(error)
+      console.log(error);
+      
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Demo App to test AWS Amplify {isLoading ? <ActivityIndicator/> : null}</Text>
+      
+
+      <StatusBar style="auto" />
+      <View style={{borderWidth:1}}>
+
+        <Button onPress={addBlogFxn} title="Add Blog"/>
+        <Button onPress={listBlogsFxn} title="List Blogs"/>
+      </View>
+
+
+      <View style={{borderWidth:1}}>
+        <Text>List of blogs titles</Text>
+        {blogs.map( (value, index) => <Text>{value}</Text>)}
+      </View>
+      <View style={{borderWidth:1}}>
+        <Text>Error message:  {error.message}</Text>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "gtibdsbackend": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
